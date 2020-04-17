70ddb9191d478fbe95b0ced3805e47c10d076974
Jordan Hewitt <hewitj2@gmail.com>
2020-04-16 23:48:55 -0700

    trying this way

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import FlashcardList from './FlashcardList';
-import "./app.css";
+// import "./app.css";
 import axios from 'axios';
 
 function App() {
